f1ac7d6b9bb540fef9009cf6adfb5f894d863f73
vicentecom <vicente@comunitea.com>
2021-08-27 10:23:11 +0200

    [IMP] website_custom_bta: traducciones

--- website_custom_bta/models/mail_mail.py
+++ website_custom_bta/models/mail_mail.py
@@ -1,7 +1,7 @@
 
 # © 2021 Comunitea
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import models
+from odoo import models, _
 
 
 class MailMail(models.AbstractModel):
@@ -14,10 +14,11 @@ class MailMail(models.AbstractModel):
         base_url = get_param('web.base.url')
         list_id = get_param('website_custom_bta.list_id')
 
-        newsletter_string = '<tr><td align="center" style="min-width: 590px; padding: 8px; font-size:11px;"><a target="_blank" href="{}/website_custom_bta/subscribe?list_id={}&email={}" style="color: #875A7B;">Subscribe to our newsletter</a></td></tr>'.format(
+        newsletter_string = '<tr><td align="center" style="min-width: 590px; padding: 8px; font-size:11px;"><a target="_blank" href="{}/website_custom_bta/subscribe?list_id={}&email={}" style="color: #875A7B;">{}</a></td></tr>'.format(
             base_url,
             list_id,
             partner.email if partner else self.email_to,
+            _('Subscribe to our newsletter'),
         )        
 
         position = res['body'].rfind('</table>')
